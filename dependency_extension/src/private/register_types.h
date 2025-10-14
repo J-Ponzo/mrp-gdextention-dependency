634f398c0b898a9e5f306772ef649ceda3fc1c5d
jponzo <jeremy.ponzo@gmail.com>
2025-10-14 22:33:44 +0200

    drop linux best effort and try force dll loading

--- dependency_extension/src/private/register_types.h
+++ dependency_extension/src/private/register_types.h
@@ -1,5 +1,5 @@
 #ifndef DEPENDENCY_REGISTER_TYPES_H
-#define GDEXAMPLE_REGISTER_TYPES_H
+#define DEPENDENCY_REGISTER_TYPES_H
 
 #include <godot_cpp/core/class_db.hpp>
 
@@ -8,4 +8,4 @@ using namespace godot;
 void initialize_dependency_module(ModuleInitializationLevel p_level);
 void uninitialize_dependency_module(ModuleInitializationLevel p_level);
 
-#endif // GDEXAMPLE_REGISTER_TYPES_H
+#endif // DEPENDENCY_REGISTER_TYPES_H
--- main_extension/src/private/register_types.cpp
+++ main_extension/src/private/register_types.cpp
@@ -5,6 +5,10 @@
 #include <gdextension_interface.h>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/project_settings.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+#include <windows.h>
 
 using namespace godot;
 
@@ -22,9 +26,18 @@ void uninitialize_main_module(ModuleInitializationLevel p_level) {
 	}
 }
 
+void add_dll_search_dir() {
+	String res_path = "res://dependency_extension/bin/";
+	String abs_path = ProjectSettings::get_singleton()->globalize_path(res_path);
+    SetDllDirectory(abs_path.utf8().get_data());
+	UtilityFunctions::print("Add dependency folder to dll search folders : ", abs_path.utf8().get_data());
+}
+
 extern "C" {
 // Initialization.
 GDExtensionBool GDE_EXPORT main_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	add_dll_search_dir();
+	
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 	init_obj.register_initializer(initialize_main_module);
